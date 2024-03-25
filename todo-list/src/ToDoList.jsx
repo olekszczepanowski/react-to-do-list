b465840d897d4a0461740e0903c4c3e1ffc125f9
olekszczepanowski <olekszczepanowski04@wp.pl>
2024-03-25 21:27:48 +0100

    TODO: theme swapper and input-container

--- todo-list/src/ToDoList.jsx
+++ todo-list/src/ToDoList.jsx
@@ -36,23 +36,36 @@ function ToDoList(){
         <div id="input-container">
             <input type="text" placeholder="Type in your task!" id="text-input" onChange={changeName}></input>
             <input type="date" id="calendar-input" onChange={changeDate}></input>
-            <button onClick={addTask}>Add</button>
+            <button id="btn-task-add" onClick={addTask}><span class="material-symbols-outlined">add</span></button>
         </div>
         <div id="tasks-list">
             <h2>Tasks:</h2>
             <ul>
                 {tasks.map((task,index) => 
-                <li key={index}><span className='task-name'>{task.name} {task.date}</span>
-                <button id="btn-task-done" onClick={() => (addToCompletedList(index))}>Done</button>
-                </li>)}
+                <li key={index}>
+                <span className='task-name'>{task.name}</span>
+                <div className='right-side-li'>
+                    <span className="task-date">{task.date}</span>
+                    <button className="btn-task-done" onClick={() => 
+                        (addToCompletedList(index))}>
+                            <span class="material-symbols-outlined">check</span>
+                            </button>
+                </div></li>)}
             </ul>
         </div>
         <div id="completed-tasks-list">
             <h2>Completed tasks:</h2>
             <ul>
                 {completedTasks.map((task,index) => 
-                <li key={index}><span className='task-name'>{task.name} {task.date}</span>
-                <button id="btn-task-delete" onClick={() => (removeCompletedTask(index))}>Delete</button></li>)}
+                <li key={index}>
+                <span className='task-name'>{task.name}</span>
+                <div className='right-side-li'>
+                    <span className="task-date">{task.date}</span>
+                    <button className="btn-task-delete" onClick={() => 
+                        (removeCompletedTask(index))}><span class="material-symbols-outlined">close</span>
+                        </button>
+                </div>
+                </li>)}
             </ul>
         </div>
     </div>)
